6dbc4222e4a85335519b8489ac018d5ec62fe595
Rohit Goyal <irohitgoyal@gmail.com>
2025-10-04 17:33:54 +0530

    Update ZuulProxyApplication.java

--- src/main/java/com/zuul/ZuulProxyApplication.java
+++ src/main/java/com/zuul/ZuulProxyApplication.java
@@ -3,15 +3,12 @@ package com.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-// This is a spring boot application Hence @SpringBootApplication annotation is required
+// This is a spring boot application
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulProxyApplication {
-
 	public static void main(String[] args) {
 		System.out.println("Starting Application");
 		SpringApplication.run(ZuulProxyApplication.class, args);
 	}
-
 }
